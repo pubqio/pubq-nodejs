be32b0bdcfc57ac701eaeb00b58fae46e126b38f
A.J <ali.jamalzad@gmail.com>
2023-05-24 03:27:40 +0330

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import axios, { AxiosInstance } from "axios";
+
+class HttpApi {
+    private applicationId: string;
+    private applicationKey: string;
+    private applicationSecret: string;
+    private httpClient: AxiosInstance;
+
+    constructor(
+        applicationId: string,
+        applicationKey: string,
+        applicationSecret: string
+    ) {
+        this.applicationId = applicationId;
+        this.applicationKey = applicationKey;
+        this.applicationSecret = applicationSecret;
+
+        this.httpClient = axios.create({
+            baseURL: "https://api.pubq.io",
+        });
+    }
+
+    public async publish(channel: string, data: string | any[]): Promise<any> {
+        const response = await this.httpClient.post(
+            "/v1/messages/publish",
+            {
+                channel,
+                data,
+            },
+            {
+                headers: {
+                    Id: this.applicationId,
+                    Key: this.applicationKey,
+                    Secret: this.applicationSecret,
+                },
+            }
+        );
+
+        return response.data;
+    }
+}
+
+export { HttpApi };
